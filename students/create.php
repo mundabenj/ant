383d01017b1751fb5f02fd36da7d5403a3cbb687
ALPHAWOLF\munda <benjamin.mundama@strathmore.edu>
2025-11-05 16:56:48 +0300

    created a create API

---
+++ students/create.php
@@ -0,0 +1,81 @@
+<?php
+// JSON Headers
+header('Content-Type: application/json'); // define content type as JSON
+header('Access-Control-Allow-Origin: *'); // allow access from any origin
+header('Access-Control-Allow-Methods: POST'); // allow POST method
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Authorization, X-Requested-With'); // allow specific headers  
+
+// Verify that the request method is POST
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $response = [
+        'status'=> http_response_code(405), // Method Not Allowed
+        'message'=> 'Method Not Allowed. Use POST.',
+    ];
+    echo json_encode($response);
+    exit();
+}
+
+// Require database connection
+require_once '../inc/pdo.php';
+
+// Get the raw POST data
+$requestBody = file_get_contents("php://input"); // for JSON data
+
+// Decode the JSON data
+$JSONData = json_decode($requestBody, true); // associative array
+
+// Retrieve form data
+$FORMData = [
+    'fullname' => $_POST['fullname'] ?? null,
+    'email' => $_POST['email'] ?? null
+];
+
+// Choose between JSON and Form Data
+if(!empty($JSONData)) {
+    $requestData = $JSONData;
+} else {
+    $requestData = $FORMData;
+}
+
+// Validate required fields
+if(empty($requestData['fullname']) || empty($requestData['email'])) {
+    $response = [
+        'status'=> http_response_code(400), // Bad Request
+        'message'=> 'Bad Request. fullname and email are required.',
+    ];
+    echo json_encode($response);
+    exit();
+}
+
+// prepare an insert statement
+try {
+    $sql = "INSERT INTO users (fullname, email) VALUES (:fullname, :email)";
+    $stmt = $pdo->prepare($sql);
+
+    // bind parameters
+    $stmt->bindParam(':fullname', $requestData['fullname'], PDO::PARAM_STR);
+    $stmt->bindParam(':email', $requestData['email'], PDO::PARAM_STR);
+
+    // execute the statement
+    $stmt->execute();
+
+    // success response
+    $response = [
+        'status'=> http_response_code(201), // Created
+        'message'=> 'Student created successfully.',
+        'StudentData' =>[
+            'userId' => $pdo->lastInsertId(),
+            'fullname' => $requestData['fullname'],
+            'email' => $requestData['email']
+        ]
+    ];
+    echo json_encode($response);
+} catch (PDOException $e) {
+    // error response
+    $response = [
+        'status'=> http_response_code(500), // Internal Server Error
+        'message'=> 'Internal Server Error. ' . $e->getMessage(),
+    ];
+    echo json_encode($response);
+    exit();
+}
